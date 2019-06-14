ed8391555131528f0ef7d8df244265ba213ddbc3
dionolympia <dionolympia@gmail.com>
2019-06-14 03:19:36 -0400

    Avatar + new Gradient + Caption

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 
+import Background from "./home-background.jpg";
+import Avatar from "./avatar.png";
+
 class Home extends Component {
   state = {};
   render() {
     return (
-      <div style={{ width: "100%", margin: "auto" }}>
-        <Grid className="landingGrid">
+      <div style={{ margin: "auto", width: "100%" }}>
+        <Grid className="Home-page">
           <Cell col={12}>
-            <h1 style={{ textAlign: "center" }}> Hello World </h1>
+            <img style={{ display: "block", margin: "auto" }} src={Avatar} />
+            <br />
+            <div className="Avatar-caption">
+              <h1 className="Caption-content">Software Developer</h1>
+              <p className="Caption-content">
+                HTML | CSS | JavaScript | C++ | C# | Python | Java | React |
+                MySQL | Swift
+              </p>
+              <h1 className="Caption-content">Icons</h1>
+            </div>
           </Cell>
         </Grid>
       </div>
