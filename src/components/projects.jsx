c57fcd5fcd310b57aebe5dfabf036e5c3c2d43f1
dionolympia <dionolympia@gmail.com>
2019-06-29 02:27:52 -0400

    projects page finishing touches

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -19,8 +19,8 @@ class Projects extends React.Component {
                   Personal Portfolio website made with React.  Implemented with React Bootstrap, React Router V4, & Material Lite UI.
                 </CardText>
                 <CardActions border>
-                    <Button colored>Website</Button>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://dionolympia.github.io/portfolio" colored>Website</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/portfolio" colored>Github</Button>
                 </CardActions>
               </Card>
           </Cell>
@@ -31,8 +31,8 @@ class Projects extends React.Component {
                     Basic counter web app created to practice basic concepts in React including: JSX, Rendering to the DOM, & Handling Events
                 </CardText>
                 <CardActions border>
-                    <Button colored>Website</Button>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://dionolympia.github.io/counter" colored>Website</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/counter" colored>Github</Button>
                 </CardActions>
               </Card>
           </Cell>
@@ -50,8 +50,8 @@ class Projects extends React.Component {
                   2D side-scroller, adventure iOS game developed in Unity.
                 </CardText>
                 <CardActions border>
-                    <Button colored>Website</Button>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/counter" colored>Website</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/counter" colored>Github</Button>
                 </CardActions>
               </Card>
           </Cell>
@@ -62,8 +62,7 @@ class Projects extends React.Component {
                     Side-scroller arcade iOS game based on "Flappy Bird" with an added twist.
                 </CardText>
                 <CardActions border>
-                    <Button colored>Website</Button>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/flapgoesthebirdie" colored>Github</Button>
                 </CardActions>
               </Card>
           </Cell>
@@ -77,11 +76,13 @@ class Projects extends React.Component {
               <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
                 <CardTitle expand style={{color: '#fff', background: 'url(https://dionolympia.github.io/portfolio/project-images/tetris.png) center / cover '}}>Zero Gravity Tetris</CardTitle>
                 <CardText>
-                    
+                  Tile-matching puzzle game based on the original “Tetris”.
+                  Game logic written in JavaScript and webpage design written in HTML & CSS, utilizing Canvas API.
                 </CardText>
                 <CardActions border>
-                    <Button colored>Website</Button>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://dionolympia.github.io/ZGTetris/" colored>Website</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/ZGTetris" colored>Github</Button>
+
                 </CardActions>
               </Card>
           </Cell>
@@ -89,12 +90,12 @@ class Projects extends React.Component {
               <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
                 <CardTitle expand style={{color: '#000', background: 'url(https://dionolympia.github.io/portfolio/project-images/life.png) center / cover'}}>Game of Life</CardTitle>
                 <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aenan convallis.
+                  Collaborative course project implementing Conway's Game of Life as a web application.
+                  Webpages developed in PHP, HTML, & CSS, while game logic written in JavaScript.
                 </CardText>
                 <CardActions border>
-                    <Button colored>Website</Button>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://game-of-life-dion-olympia.herokuapp.com/" colored>Website</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/game-of-life" colored>Github</Button>
                 </CardActions>
               </Card>
           </Cell>
@@ -102,11 +103,11 @@ class Projects extends React.Component {
               <Card shadow={0} style={{width: '320px', height: '320px', margin: 'auto'}}>
                 <CardTitle expand style={{color: '#fff', background: 'url(https://dionolympia.github.io/portfolio/project-images/ben.png) center / cover '}}>Ben 10 Battle</CardTitle>
                 <CardText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aenan convallis.
+                  Text-based fighting simulation game that used characters from “Ben 10”, coded in C++.
+
                 </CardText>
                 <CardActions border>
-                    <Button colored>Github</Button>
+                    <Button style={{color: "#000"}} target="_blank" href="https://github.com/dionolympia/Ben10Battle" colored>Github</Button>
                 </CardActions>
               </Card>
           </Cell>
