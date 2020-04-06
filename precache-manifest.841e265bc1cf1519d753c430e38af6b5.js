09999492eaabed3fe02951f49c1f3f40a28d6134
Dion Olympia <dionolympia@gmail.com>
2020-04-05 20:40:07 -0400

    Updates

---
+++ precache-manifest.841e265bc1cf1519d753c430e38af6b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca0dfda0a235e18d89ce8c18ace8886",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5249322a023545826bbf",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "728750af7b0af0e1fb05",
+    "url": "/static/css/main.dc1d76d7.chunk.css"
+  },
+  {
+    "revision": "5249322a023545826bbf",
+    "url": "/static/js/2.e7733bf0.chunk.js"
+  },
+  {
+    "revision": "d5ac4d7b0dfedd09f1b15b52a47d99ce",
+    "url": "/static/js/2.e7733bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728750af7b0af0e1fb05",
+    "url": "/static/js/main.6ead5098.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "a3568c24a77a279e87ed4e14a5aa1083",
+    "url": "/static/media/avatar.a3568c24.jpeg"
+  },
+  {
+    "revision": "f9afff17341cbf9c3ce209e03dd02f9f",
+    "url": "/static/media/game-of-life.f9afff17.png"
+  },
+  {
+    "revision": "795e22b6cdfb69058b9130c126f301d3",
+    "url": "/static/media/resume.795e22b6.pdf"
+  },
+  {
+    "revision": "009448851a97aa0a0d74522d51347518",
+    "url": "/static/media/tetris.00944885.png"
+  },
+  {
+    "revision": "450eb093ed30f24286998fbaf5354576",
+    "url": "/static/media/wanderer.450eb093.png"
+  }
+]);
--- src/components/main.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Home from "./home.jsx";
-
-const Main = () => {
-  return <Home />;
-};
-
-export default Main;
